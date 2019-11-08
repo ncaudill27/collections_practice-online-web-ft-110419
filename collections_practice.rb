46e1722b5f921625ef51026c8165e4bbca74185e
Nelson A Caudill <nelson.a.caudill@gmail.com>
2019-11-08 17:37:40 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,15 @@
+def sort_array_asc(array)
+  array.sort
+end
 
+def sort_array_desc(array)
+  array.sort { |x,y| y <=> x }
+end
+
+def sort_array_char_count(array)
+  array.sort { |x,y| x.size <=> y.size }
+end
+
+def swap_elements(array)
+  array.sort { |x,y| array[1] <=> array[2] }
+end
